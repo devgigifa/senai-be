24c59ab5ba26b16bffa4b27306b214b6c49995d8
GIOVANNA FONSECA ALVES <giovanna_f_alves@estudante.sesisenai.org.br>
2024-10-23 16:23:25 -0300

    upload server-api

---
+++ server-api/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+app.get('/sobre', (req, res) => {
+    res.send("Sobre")
+})
+app.listen(port, () => {
+    console.log("Servidor rodando em: http://localhost:" + port)
+})
